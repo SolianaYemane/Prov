4c335277d78594f7b23aea785d10f9f0ded68a66
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-01-05 10:57:21 -0800

    Update DemoS.java

--- src/prov/DemoS.java
+++ src/prov/DemoS.java
@@ -10,4 +10,8 @@ public class DemoS {
 	public static void add(int a, int b){
 		System.out.println(a+b);
 	} 
+	//Good Job Soliana 
+	public static void add(int a, int b,int c){
+		System.out.println(a+b+c); 
+	} 
 }
